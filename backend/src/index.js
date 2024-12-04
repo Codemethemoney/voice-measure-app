ec31c6db5d13c86838d46b2a682c697c472b36f3
Gary OLeary <161914284+Codemethemoney@users.noreply.github.com>
2024-12-04 00:45:16 -0600

    Add Swagger API documentation setup

---
+++ backend/src/index.js
@@ -0,0 +1,8 @@
+// Existing code
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Existing code
